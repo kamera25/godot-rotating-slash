9fbfaa15d4c5261890f7cc015d3b6d0965e981ff
NAKAOKU Takahiro <giraffe_rakugaki@yahoo.co.jp>
2024-09-22 01:18:58 +0900

    とりあえず完成とする

--- Script/SkallBehavior.gd
+++ Script/SkallBehavior.gd
@@ -8,6 +8,9 @@ var _direction : Vector2
 
 
 func _ready() -> void:
+	
+	_direction.y = 0.1
+	
 	# 1秒毎に移動の向きを変える処理
 	var timer = Timer.new()
 	timer.timeout.connect( Callable(self, "_set_move_direction"))
@@ -17,18 +20,21 @@ func _ready() -> void:
 	
 	
 
-func _process(_delta) -> void:
-	_animated_sprite.play("walk_right")
-#	if Input.is_action_pressed("ui_right"):
-#		_animated_sprite.play("walk_right")
-#	elif Input.is_action_pressed("ui_left"):
-#		_animated_sprite.play("walk_left")
-#	elif Input.is_action_pressed("ui_up"):
-#		_animated_sprite.play("walk_forward")
-#	elif Input.is_action_pressed("ui_down"):
-#		_animated_sprite.play("walk_back")
-#	else:
-#		_animated_sprite.stop()
+func _process(_delta) -> void:	
+	# キャラクターの向きに応じて、アニメーションを変更する
+	var is_move_horizontal : bool = absf( _direction.x ) > absf( _direction.y )
+	
+	if is_move_horizontal:
+		if _direction.x > 0:
+			_animated_sprite.play("walk_right")
+		else :
+			_animated_sprite.play("walk_left")
+	else:
+		if _direction.y > 0:
+			_animated_sprite.play("walk_back")
+		else :
+			_animated_sprite.play("walk_forward")
+	
 
 # 移動する向きをメンバ変数に代入する
 func _set_move_direction() -> void:
@@ -40,12 +46,10 @@ func _get_random_direction() -> Vector2:
 	direction.x = randf() * 2.0 - 1.0
 	direction.y = randf() * 2.0 - 1.0
 	
-	print("hello")
 	return direction
 
 
 func _physics_process( _delta: float) -> void:
-
 	velocity.x = _direction.x * SPEED
 	velocity.y = _direction.y * SPEED
 		
